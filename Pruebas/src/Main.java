cc41051c6df1afc8b13cdbf25af04e89c7544675
SamCab28 <samircabrera2528@gmail.com>
2023-09-29 10:47:22 -0600

    prueba Intelliji

---
+++ Pruebas/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
+        
+        int i = 20;
+        int j = 3;
+        int result = 0;
+        
+        try{
+            result = i/j;
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+        
+        System.out.println(result);
+    }
+}
